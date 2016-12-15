5efbb788afd46c103ad629269f3419ab98d4cd48
Robert Kaiser <kairo@kairo.at>
2016-12-16 00:28:16 +0100

    make the piwik code work with IE at least down to 8

--- app/piwik.js
+++ app/piwik.js
@@ -4,10 +4,10 @@
   _paq.push(['enableLinkTracking']);
   (function() {
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    // dataset does not work with IE10 or lower :(
-    var u=s.parentNode.dataset.piwikurl;
+    // dataset does not work with IE10 or lower, so work around it with .getAttribute() :(
+    var u=(s.parentNode.dataset?s.parentNode.dataset.piwikurl:s.parentNode.getAttribute('data-piwikurl'));
     _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', s.parentNode.dataset.piwiksite]);
+    _paq.push(['setSiteId', (s.parentNode.dataset?s.parentNode.dataset.piwiksite:s.parentNode.getAttribute('data-piwiksite'))]);
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
 // End Piwik code

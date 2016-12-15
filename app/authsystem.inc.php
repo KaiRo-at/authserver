7165d4fd908dc7b40270731fa9f099a3f6fda6e1
Robert Kaiser <kairo@kairo.at>
2016-12-15 22:28:18 +0100

    KaiRo bug 413 - Set up doctrine DBAL to drive the DB and do schema migration

--- app/authsystem.inc.php
+++ app/authsystem.inc.php
@@ -41,40 +41,6 @@ $settings['piwik_site_id'] = intval(@$settings['piwik_site_id']);
 $settings['piwik_url'] = strlen(@$settings['piwik_url']) ? $settings['piwik_url'] : '/piwik/';
 $settings['piwik_tracker_path'] = strlen(@$settings['piwik_tracker_path']) ? $settings['piwik_tracker_path'] : '../vendor/piwik/piwik-php-tracker/';
 
-/* Creating the DB tables:
-CREATE TABLE `auth_sessions` (
- `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT ,
- `sesskey` VARCHAR(150) NOT NULL DEFAULT '' ,
- `user` MEDIUMINT UNSIGNED NULL DEFAULT NULL ,
- `logged_in` BOOLEAN NOT NULL DEFAULT FALSE ,
- `time_created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
- `time_expire` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
- `saved_redirect` VARCHAR(255) NOT NULL DEFAULT '' ,
- PRIMARY KEY (`id`),
- INDEX (`sesskey`),
- INDEX (`time_expire`)
-);
-CREATE TABLE `auth_users` (
- `id` MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT ,
- `email` VARCHAR(255) NOT NULL ,
- `pwdhash` VARCHAR(255) NOT NULL ,
- `status` ENUM('unverified','ok') NOT NULL DEFAULT 'unverified' ,
- `verify_hash` VARCHAR(150) NULL DEFAULT NULL ,
- `group_id` MEDIUMINT UNSIGNED DEFAULT '0' ,
- PRIMARY KEY (`id`),
- UNIQUE (`email`)
-);
-CREATE TABLE `auth_log` (
- `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT ,
- `code` VARCHAR(100) NOT NULL ,
- `info` TEXT NULL DEFAULT NULL ,
- `ip_addr` VARCHAR(50) NULL DEFAULT NULL ,
- `time_logged` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
- PRIMARY KEY (`id`),
- INDEX (`time_logged`)
-);
-*/
-
 // Set up our OAuth2 Server object
 $server = $utils->getOAuthServer();
 ?>
